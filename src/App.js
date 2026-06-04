0817f07a6209e5b621a377f5432bf4d973700f3a
Maximillian Eldrich Zhu <eldrichzhu@gmail.com>
2026-06-04 11:51:32 +0700

    Learning react

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useState, useEffect } from 'react';
 import { supabase } from './supabaseClient'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count , setCount] = useState(0)
+  const [difference, setDifference] = useState(0)
   const [data, setData] = useState([])
   const [users, setUsers] = useState([])
   
@@ -22,7 +23,14 @@ function App() {
   return <div>
     {data.map(item => <p key={item.id}>{item.title}</p>)}
     <p>Count: {count}</p>
-    <button onClick={() => setCount(count + 1)}>Add</button>
+    <button onClick={() => setCount(count + difference)}>Add</button>
+    <br></br><br></br>
+    <button onClick={() => setDifference(difference + 1)}>+</button>
+    <p>Add by: {difference}</p>
+    <button onClick={() => setDifference(difference - 1)}>-</button>
+    <br></br><br></br>
+    <button onClick={() => {setCount(0); setDifference(0);}}>Reset</button>
+    <br></br>
     <p>Check console for Supabase Connection</p>
   </div>
 }
